66f14bcf627b0be941f6e5019bc5892df833ca84
chidrome <chidrome@gmail.com>
2019-07-27 23:14:54 -0700

    added login post route. manually retrieving known user in the code since there is no hasing involved

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+const express = require('express');
+const AWS = require('aws-sdk');
+const expressJwt = require('express-jwt');
+
+// env variables
+require('dotenv').config();
+
+// app/middleware setup
+const app = express();
+app.use(express.urlencoded({extended: false}));
+
+
+// Helper function: This allows our server to parse the incoming token from the client
+// This is being run as middleware, so it has access to the incoming request
+function fromRequest(req){
+  console.log('hello', req.body);
+  if(req.body.headers &&
+    req.body.headers.Authorization &&
+    req.body.headers.Authorization.split(' ')[0] === 'Bearer'){
+      return req.body.headers.Authorization.split(' ')[1];
+  }
+  return null;
+}
+
+app.get('/', (req, res)=> {
+  res.send('Home page stub route.')
+})
+app.use('/auth', expressJwt({
+  secret: process.env.JWT_SECRET,
+  getToken: fromRequest
+}).unless({
+  path: [{ url: '/auth/login', methods: ['POST'] }, { url: '/auth/data', methods: ['GET'] }]
+}), require('./controllers/auth'));
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log("🎵 Now listening to the smooth sounds of port 3000🍺  🎵")
+});
+
